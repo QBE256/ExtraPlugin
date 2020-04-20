94fa1ef9de65931ff21847b1241a235bc6832546
QBE256 <55350808+QBE256@users.noreply.github.com>
2020-04-20 14:41:15 +0900

    Update 手抜き.js

--- 手抜き.js
+++ 手抜き.js
@@ -1,21 +1,18 @@
 // AIカスタムで"PriorityWait"をキーワードに設定する
 // 移動後攻撃不可武器だけを持っている敵ならこのAIの方が向いている
-// 待機時に攻撃できるなら待機したまま攻撃してくれる
-// 攻撃できない場合は移動だけ行う
+// 待機時に攻撃できるなら待機したまま攻撃&できない時は移動して攻撃
 AutoActionBuilder._buildPriorityWaitAction = function(unit, autoActionArray) {
 	var combination;
 
 	// 現在位置から攻撃可能なユニットの中で、最も優れた組み合わせを取得する
 	combination = CombinationManager.getWaitCombination(unit);
+
 	if (combination === null) {
-		combination = CombinationManager.getEstimateCombination(unit);
-		if (combination === null) {
-			return this._buildEmptyAction();
-		}
-		else {
-			this._pushMove(unit, autoActionArray, combination);
-			this._pushWait(unit, autoActionArray, combination);
-		}
+		combination = CombinationManager.getApproachCombination(unit, true);
+	}
+
+	if (combination === null) {
+		return this._buildEmptyAction();
 	}
 	else {
 		this._pushGeneral(unit, autoActionArray, combination);
