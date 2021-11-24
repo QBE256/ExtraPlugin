f7bb0147d660a2bc628bf0b6a609d7158f1c77f5
QBE256 <55350808+QBE256@users.noreply.github.com>
2021-11-25 02:02:42 +0900

    Update ShowWeaponType.js

--- ShowWeaponType.js
+++ ShowWeaponType.js
@@ -1,5 +1,5 @@
 /*--------------------------------------------------------------------------
-　武器の情報ウィンドウに武器タイプを表示する ver 1.0
+　武器の情報ウィンドウに武器タイプを表示する ver 1.1
 
 ■作成者
 キュウブ
@@ -8,6 +8,10 @@
 このスクリプトを導入すると武器の情報ウィンドウに武器タイプが表示されるようになります。
 
 ■更新履歴
+ver 1.1 2021/11/25
+インデントが誤っていた箇所や無駄なスペースを修正
+(挙動はver1.0と全く変わらないです)
+
 ver 1.0 2021/11/24
 公開
 
@@ -31,10 +35,10 @@ ItemSentence.WeaponType = defineObject(BaseItemSentence,
 		var handle = item.getWeaponType().getIconResourceHandle();
 
 		ItemInfoRenderer.drawKeyword(x, y, "タイプ");
-		x += ItemInfoRenderer.getSpaceX();	
 		if (handle.isNullHandle()) {
 			return;
 		}
+		x += ItemInfoRenderer.getSpaceX();
 		GraphicsRenderer.drawImage(x, y, handle, GraphicsType.ICON);
 	},
 	
@@ -55,5 +59,5 @@ ItemSentence.WeaponType = defineObject(BaseItemSentence,
 	ItemInfoWindow._configureItem = function(groupArray) {
 		groupArray.appendObject(ItemSentence.WeaponType);
 		_ItemInfoWindow__configureItem.apply(this, arguments);
-    };
+	};
 })();
