1eea556ad8e65c57c3c7db9eb9862af1cc896b28
QBE256 <55350808+QBE256@users.noreply.github.com>
2021-12-05 14:30:02 +0900

    Update 敵ランダム武器選択.js

--- 敵ランダム武器選択.js
+++ 敵ランダム武器選択.js
@@ -1,30 +1,36 @@
 /*
-	敵の武器選択ロジックがランダムになる
+	敵の武器選択ロジックがランダムになる ver1.1
 
 	■作成者
 	キュウブ
 
-	CPUスコア計算でダメージ量や命中率などを無視して、トータルスコアがランダムになる
-	よって、どの武器を選択するかもランダムになる
+	CPUのカスタムパラメータにisRandomSelect:trueと記しておくと
+	攻撃時の武器を選択がランダムになる
 	※コンフィグでダメージ0、命中率0を無視する設定だけは有効
 
 	スコアは30点～80点のいずれかになる
 
-	
-*/
-AIScorer.Weapon._getTotalScore = function(unit, combination) {
-	if (
-		!DataConfig.isAIDamageZeroAllowed() &&
-		this._getDamageScore(unit, combination) === 0
-	) {
-		return -1;
-	}
-	if (
-		!DataConfig.isAIHitZeroAllowed() &&
-		this._getHitScore(unit, combination)
-	) {
-		return -1;
-	}
 
-	return root.getRandomNumber() % 50 + 30;
-};
+*/
+(function(){
+	var _AIScorer_Weapon__getTotalScore = AIScorer.Weapon._getTotalScore;
+	AIScorer.Weapon._getTotalScore = function(unit, combination) {
+		if (unit.custom.isRandomSelect !== true) {
+			return _AIScorer_Weapon__getTotalScore.apply(this, arguments);
+		}
+		if (
+			!DataConfig.isAIDamageZeroAllowed() &&
+			this._getDamageScore(unit, combination) === 0
+		) {
+			return -1;
+		}
+		if (
+			!DataConfig.isAIHitZeroAllowed() &&
+			this._getHitScore(unit, combination)
+		) {
+			return -1;
+		}
+
+		return root.getRandomNumber() % 50 + 30;
+	};
+})();
