1d2f2e807f9f5b518bee7fe3e7b4f8c8e59a40f5
QBE256 <55350808+QBE256@users.noreply.github.com>
2022-02-26 22:29:22 +0900

    Update test.js

--- test.js
+++ test.js
@@ -1,13 +1,27 @@
-UnitCommand.Metamorphoze.endCommandAction = function() {
-	this._listCommandManager.endCommandAction(null);
-};
-
-(function(){
-	var _UnitCommand_Metamorphoze_isCommandDisplayable = UnitCommand.Metamorphoze.isCommandDisplayable;
-	UnitCommand.Metamorphoze.isCommandDisplayable = function() {
-		if (this.getCommandTarget().getMostResentMov() > 0) {
-			return false;
+// カスパラにisOverlapping:trueと記したステートは重複させる事が可能になる
+// ただし、1ユニットが持てるステートの上限数(6つ)を超えて持つことはできないので注意
+StateControl.arrangeState = function(unit, state, increaseType) {
+	var turnState;
+	var list = unit.getTurnStateList();
+	var count = list.getCount();
+	var editor = root.getDataEditor();
+		
+	if (increaseType === IncreaseType.INCREASE) {
+		turnState = this.getTurnState(unit, state);
+		if (turnState.custom.isOverlapping !== true && turnState !== null) {
+			// 既にステートが追加されている場合は、ターン数値を更新する
+			turnState.setTurn(state.getTurn());
 		}
-		return _UnitCommand_Metamorphoze_isCommandDisplayable.apply(this, arguments);
-	};
-})();
+		else {
+			if (count < DataConfig.getMaxStateCount()) {
+				editor.addTurnStateData(list, state);
+			}
+		}
+	}
+	else if (increaseType === IncreaseType.DECREASE) {
+		editor.deleteTurnStateData(list, state);
+	}
+	else if (increaseType === IncreaseType.ALLRELEASE) {
+		editor.deleteAllTurnStateData(list);
+	}
+};
