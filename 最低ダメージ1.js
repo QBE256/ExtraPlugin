6e3ae6c02c0f9f0dba4aebde9a00ad3bef67a0c1
QBE256 <55350808+QBE256@users.noreply.github.com>
2022-06-17 23:56:40 +0900

    Update 最低ダメージ1.js

--- 最低ダメージ1.js
+++ 最低ダメージ1.js
@@ -1,4 +1,33 @@
 // 最低ダメージが1になります 以上
-DefineControl.getMinDamage = function() {
+DefineControl.getMinDamage = function () {
   return 1;
+};
+
+AttackEvaluator.ActiveAction._arrangePassiveDamage = function (
+  virtualActive,
+  virtualPassive,
+  attackEntry
+) {
+  var damagePassive = attackEntry.damagePassive;
+  var value = this._getDamageGuardValue(
+    virtualActive,
+    virtualPassive,
+    attackEntry
+  );
+
+  if (value !== -1) {
+    value = 100 - value;
+    damagePassive = Math.floor(damagePassive * (value / 100));
+    if (minDamage > damagePassive) {
+      damagePassive = minDamage;
+    }
+  }
+
+  if (virtualPassive.hp - damagePassive < 0) {
+    damagePassive = virtualPassive.hp;
+  } else {
+    attackEntry.damagePassiveFull = damagePassive;
+  }
+
+  return damagePassive;
 };
