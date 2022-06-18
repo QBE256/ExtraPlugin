671c2b7cbfbfe795e005cd6fb9848f5470bc75af
QBE256 <55350808+QBE256@users.noreply.github.com>
2022-06-18 22:05:21 +0900

    Update 最低ダメージ1.js

--- 最低ダメージ1.js
+++ 最低ダメージ1.js
@@ -1,4 +1,6 @@
-// 最低ダメージが1になります 以上
+// 最低ダメージが1になります
+// ダメージガードスキルは軽減率を100%にした場合0ダメージ
+// 99%以下の場合は1ダメージが保障されます
 DefineControl.getMinDamage = function () {
   return 1;
 };
@@ -20,7 +22,8 @@ AttackEvaluator.ActiveAction._arrangePassiveDamage = function (
     value = 100 - value;
     damagePassive = Math.floor(damagePassive * (value / 100));
     minDamage = DefineControl.getMinDamage();
-    if (minDamage > damagePassive) {
+    enableMinDamage = minDamage > damagePassive && value === 0;
+    if (enableMinDamage) {
       damagePassive = minDamage;
     }
   }
