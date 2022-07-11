a03bd3a448f5535a5f28fd1aaaa342d3f77cda43
QBE256 <55350808+QBE256@users.noreply.github.com>
2022-07-11 19:40:47 +0900

    Create CPUの行動順番割り込みAI.js

---
+++ CPUの行動順番割り込みAI.js
@@ -0,0 +1,70 @@
+/*
+ CPUの行動順番割り込みAI ver1.0
+
+ ■作成者
+キュウブ
+
+■概要
+CPUが何らかのアクションが起こせるようになったタイミングで、
+順番を無視して行動開始するようになります。
+
+例えば、
+行動順番が1番目となっている敵ヒーラーが、
+後続の誰かが負傷したときに、行動を開始するようになります。
+
+■使い方
+1.対象ユニットのAIを行動型にします（ヒーラーで無い場合は範囲内行動にチェックを入れる事を推奨）
+2.対象ユニットに以下のカスタムパラメータを付与します
+enabledInterruptOrder:true
+
+■仕様
+ユニットのアクティブイベントは順番が回ってきた時点で流れてしまいます。
+この設定を付与するユニットにアクティブイベントを設定する事は推奨しません。
+
+■更新履歴
+ver1.0 2022/07/11
+初版
+
+■対応バージョン
+SRPG Studio Version:1.161
+
+■規約
+・利用はSRPG Studioを使ったゲームに限ります。
+・商用・非商用問いません。フリーです。
+・加工等、問題ありません。
+・クレジット明記無し　OK (明記する場合は"キュウブ"でお願いします)
+・再配布、転載　OK (バグなどがあったら修正できる方はご自身で修正版を配布してもらっても構いません)
+・wiki掲載　OK
+・SRPG Studio利用規約は遵守してください。
+*/
+
+(function () {
+  var _EnemyTurn__checkNextOrderUnit = EnemyTurn._checkNextOrderUnit;
+  EnemyTurn._checkNextOrderUnit = function () {
+    var unit;
+    var list = this._getActorList();
+    var count = list.getCount();
+
+    for (var index = 0; index < count; index++) {
+      unit = list.getData(index);
+      if (this._interruptOrder(unit)) {
+        unit.setOrderMark(OrderMarkType.EXECUTED);
+        return unit;
+      }
+    }
+
+    return _EnemyTurn__checkNextOrderUnit.apply(this, arguments);
+  };
+
+  EnemyTurn._interruptOrder = function (unit) {
+    var combination;
+    var patternType = unit.getAIPattern().getPatternType();
+    var isOrderAllowed = this._isOrderAllowed(unit);
+    var isInterruptOrderAI = !!unit.custom.enabledInterruptOrder;
+    if (isOrderAllowed && isInterruptOrderAI && patternType === PatternType.APPROACH) {
+      combination = CombinationManager.getApproachCombination(unit, true);
+      return combination !== null;
+    }
+    return false;
+  };
+})();
