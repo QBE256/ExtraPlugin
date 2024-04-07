669c11ae35b9e8ff9a0a78bdcaf099a92c43f590
QBE256 <magyo111111@yahoo.co.jp>
2024-04-07 15:49:58 +0900

    Update 移動したマス目分だけ攻撃力を加算する.js

--- 移動したマス目分だけ攻撃力を加算する.js
+++ 移動したマス目分だけ攻撃力を加算する.js
@@ -1,5 +1,5 @@
 /*--------------------------------------------------------------------------
-移動したマス目分だけ攻撃力を加算する ver 1.0
+移動したマス目分だけ攻撃力を加算する ver 1.1
 
 ■作成者
 キュウブ
@@ -28,7 +28,12 @@ moveBonusCorrection: 3
 moveBonusCorrection: -1
 
 ■更新履歴
-ver1.0 2024/04/07
+ver 1.1 2024/04/07
+行動キャンセル時のエラーを解消
+武器ウィンドウに表記を追加
+その他細かい仕様変更
+
+ver 1.0 2024/04/07
 初版
 
 ■対応バージョン
@@ -45,6 +50,99 @@ SRPG Studio Version:1.287
 --------------------------------------------------------------------------*/
 
 (function () {
+  ItemControl.hasMoveBonusWeapon = function (unit) {
+    var count;
+
+    if (!unit) {
+      return null;
+    }
+
+    count = UnitItemControl.getPossessionItemCount(unit);
+
+    for (var index = 0; index < count; index++) {
+      var item = UnitItemControl.getItem(unit, index);
+      if (!!item && this.isWeaponAvailable(unit, item) && typeof item.custom.moveBonusCorrection === "number") {
+        return true;
+      }
+    }
+
+    return false;
+  };
+
+  ItemSentence.AttackAndHit.drawItemSentence = function (x, y, item) {
+    var text, moveBonusCorrection;
+    var textui = root.queryTextUI("default_window");
+    var color = textui.getColor();
+    var font = textui.getFont();
+
+    text = root.queryCommand("attack_capacity");
+    ItemInfoRenderer.drawKeyword(x, y, text);
+    moveBonusCorrection = item.custom.moveBonusCorrection;
+    if (typeof moveBonusCorrection === "number") {
+      var moveBonusSymbolPositionX = x + ItemInfoRenderer.getSpaceX() + 1;
+      var moveBonusNumberPositionX = moveBonusSymbolPositionX + 12;
+      var attackNumberPositionX = moveBonusSymbolPositionX - 14;
+      NumberRenderer.drawNumber(attackNumberPositionX, y, item.getPow());
+      if (moveBonusCorrection >= 0) {
+        TextRenderer.drawText(moveBonusSymbolPositionX, y + 6, "+", -1, color, font);
+      } else {
+        TextRenderer.drawText(moveBonusSymbolPositionX, y + 6, "-", -1, color, font);
+      }
+      NumberRenderer.drawRightMoveBonusNumber(moveBonusNumberPositionX, y, moveBonusCorrection);
+    } else {
+      var attackNumberPositionX = x + ItemInfoRenderer.getSpaceX();
+      NumberRenderer.drawRightNumber(attackNumberPositionX, y, item.getPow());
+    }
+
+    x += ItemInfoRenderer.getSpaceX() + 42;
+
+    text = root.queryCommand("hit_capacity");
+    ItemInfoRenderer.drawKeyword(x, y, text);
+    x += ItemInfoRenderer.getSpaceX();
+    NumberRenderer.drawRightNumber(x, y, item.getHit());
+  };
+
+  NumberRenderer.drawRightMoveBonusNumber = function (x, y, number) {
+    var pic = root.queryUI("number");
+    var textui = root.queryTextUI("default_window");
+    var textColor = textui.getColor();
+    var textFont = textui.getFont();
+    var width = UIFormat.NUMBER_WIDTH / 10;
+    var height = UIFormat.NUMBER_HEIGHT / 5;
+    var ySrc = 0;
+    var alpha = 255;
+
+    if (pic === null || number < 0) {
+      return;
+    }
+
+    if (number === 0) {
+      pic.drawParts(x, y, 0, ySrc, width, height);
+      return;
+    } else if (number > 1 || number < -1) {
+      var digitValue;
+      var digitCount = 0;
+      var targetNumber = number;
+      var digitArray = [];
+      while (targetNumber > 0) {
+        digitValue = Math.floor(targetNumber % 10);
+        targetNumber = Math.floor(targetNumber / 10);
+        digitArray[digitCount] = digitValue;
+        digitCount++;
+      }
+
+      for (var index = digitCount - 1; index >= 0; index--) {
+        pic.setAlpha(alpha);
+        pic.drawParts(x, y, digitArray[index] * width, ySrc, width, height);
+        x += 9;
+      }
+      x += 3;
+      TextRenderer.drawText(x, y + 6, "M", -1, textColor, textFont);
+    } else {
+      TextRenderer.drawText(x, y + 6, "M", -1, textColor, textFont);
+    }
+  };
+
   MapSequenceArea._startMove = function () {
     var cource;
     var x = this._mapCursor.getX();
@@ -61,7 +159,9 @@ SRPG Studio Version:1.287
     } else {
       // コースを作成して移動開始
       cource = this._simulateMove.createCource(this._targetUnit, x, y, this._unitRangePanel.getSimulator());
-      this._targetUnit.custom.movementValue = cource.length;
+      if (ItemControl.hasMoveBonusWeapon(this._targetUnit)) {
+        this._targetUnit.custom.movementValue = cource.length;
+      }
       this._simulateMove.startMove(this._targetUnit, cource);
     }
 
@@ -70,8 +170,9 @@ SRPG Studio Version:1.287
 
   var _MapSequenceArea__doCancelAction = MapSequenceArea._doCancelAction;
   MapSequenceArea._doCancelAction = function () {
-    var unit = this._mapCursor.getUnitFromCursor();
-    delete unit.custom.movementValue;
+    if (!!this._targetUnit) {
+      delete this._targetUnit.custom.movementValue;
+    }
     _MapSequenceArea__doCancelAction.apply(this, arguments);
   };
 
@@ -85,7 +186,9 @@ SRPG Studio Version:1.287
 
   MoveAutoAction.enterAutoAction = function () {
     var isSkipMode = this.isSkipMode();
-    this._unit.custom.movementValue = this._moveCource.length;
+    if (ItemControl.hasMoveBonusWeapon(this._targetUnit)) {
+      this._unit.custom.movementValue = this._moveCource.length;
+    }
     if (isSkipMode) {
       this._simulateMove.skipMove(this._unit, this._moveCource);
       return EnterResult.NOTENTER;
